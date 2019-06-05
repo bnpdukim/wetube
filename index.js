31d3bb9dd9f23379fc147de699dd6e6a642f6115
dukim <sajacall82@naver.com>
2019-06-05 21:06:47 +0900

    19.06.05

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log('hello')
+import express from "express";
+
+const app = express();
+const PORT = 4000;
+
+const handleListening = () => {
+    console.log(`Linstening on: http://localhost:${PORT}`);
+}
+
+const handleProfile = (req,res) => {
+    res.send("You are on my profile");
+}
+
+const handleHome = (req, res) => {
+    res.send("hello from home");
+}
+
+app.get('/',handleHome);
+
+app.get('/profile', handleProfile);
+
+app.listen(PORT, handleListening);
+
